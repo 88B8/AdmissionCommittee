d2a90bbec636d787c719823445bcc0d5558033ee
88B8 <egoboosted@proton.me>
2025-03-25 10:20:15 +0300

    Добавлены юнит тесты для всех реализаций

---
+++ AdmissionCommittee.BL.Tests/EntrantManagerTests.cs
@@ -0,0 +1,261 @@
+﻿using Moq;
+using AdmissionCommittee.BL.Contracts;
+using AdmissionCommittee.BL.Contracts.Models;
+using AdmissionCommittee.Storage.Contracts;
+using Xunit;
+using FluentAssertions;
+
+namespace AdmissionCommittee.BL.Tests
+{
+    /// <summary>
+    /// Тесты для <see cref="EntrantManager"/>
+    /// </summary>
+    public class EntrantManagerTests
+    {
+        private readonly IEntrantManager entrantManager;
+        private readonly Mock<IStorage<Entrant>> storageMock;
+        
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public EntrantManagerTests()
+        {
+            storageMock = new Mock<IStorage<Entrant>>();
+            entrantManager = new EntrantManager(storageMock.Object);
+        }
+
+        /// <summary>
+        /// Возвращает пустую коллекцию
+        /// </summary>
+        [Fact]
+        public async Task GetEntrantsShouldReturnEmpty()
+        {
+            // Arrange
+            storageMock.Setup(x => x.GetAll(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(Array.Empty<Entrant>());
+
+            // Act
+            var result = await entrantManager.GetEntrants(CancellationToken.None);
+
+            // Assert
+            result.Should().BeEmpty();
+            storageMock.Verify(x => x.GetAll(It.IsAny<CancellationToken>()), Times.Once);
+            storageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Возвращает значения
+        /// </summary>
+        [Fact]
+        public async Task GetEntrantsReturnValues()
+        {
+            // Arrange
+            var target1 = CreateEntrant();
+            var target2 = CreateEntrant();
+            storageMock.Setup(x => x.GetAll(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<Entrant>() { target1, target2 });
+
+            // Act
+            var result = await entrantManager.GetEntrants(CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeEmpty()
+                .And.HaveCount(2)
+                .And.ContainSingle(x => x.Id == target1.Id)
+                .And.ContainSingle(x => x.Id == target2.Id);
+            storageMock.Verify(x => x.GetAll(It.IsAny<CancellationToken>()), Times.Once);
+            storageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Добавляет абитуриента
+        /// </summary>
+        [Fact]
+        public async Task AddShouldWork()
+        {
+            // Arrange
+            var target = CreateEntrant();
+            var request = new EntrantRequest(
+                "Петров П.П.",
+                Gender.Male,
+                DateTime.Now.AddYears(-16),
+                EducationForm.Fulltime,
+                50,
+                60,
+                70);
+
+            storageMock.Setup(x => x.GetAll(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<Entrant>() { target });
+
+            storageMock.Setup(x => x.Add(It.IsAny<Entrant>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await entrantManager.Add(request, CancellationToken.None);
+
+            // Assert
+            storageMock.Verify(x => x.Add(It.IsAny<Entrant>(), It.IsAny<CancellationToken>()), Times.Once);
+            storageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Обрабатывает ошибку
+        /// </summary>
+        [Fact]
+        public async Task AddShouldThrow()
+        {
+            // Arrange
+            EntrantRequest? request = null;
+
+            // Act
+            Func<Task> act = () => entrantManager.Add(request, CancellationToken.None);
+
+            // Assert
+            await act.Should().ThrowAsync<ArgumentNullException>();
+        }
+
+        /// <summary>
+        /// Изменяет абитуриента
+        /// </summary>
+        [Fact]
+        public async Task EditShouldWork()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var target = CreateEntrant();
+            target.Id = id;
+
+            var request = new EntrantRequest(
+                "Петров П.П.",
+                Gender.Male,
+                DateTime.Now.AddYears(-16),
+                EducationForm.Fulltime,
+                50,
+                60,
+                70);
+
+            storageMock.Setup(x => x.Get(id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(target);
+
+            storageMock.Setup(x => x.Edit(id, It.IsAny<Entrant>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await entrantManager.Edit(id, request, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Name.Should().Be(request.Name);
+            result.Gender.Should().Be(request.Gender);
+            result.Birthday.Should().Be(request.Birthday);
+            result.EducationForm.Should().Be(request.EducationForm);
+            result.MathExamScore.Should().Be(request.MathExamScore);
+            result.RusExamScore.Should().Be(request.RusExamScore);
+            result.ITExamScore.Should().Be(request.ITExamScore);
+
+            storageMock.Verify(x => x.Get(id, It.IsAny<CancellationToken>()), Times.Once);
+            storageMock.Verify(x => x.Edit(id, It.IsAny<Entrant>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        /// <summary>
+        /// Обрабатывает ошибку
+        /// </summary>
+        [Fact]
+        public async Task EditShouldThrow()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var request = new EntrantRequest(
+                "Петров П.П.",
+                Gender.Male,
+                DateTime.Now.AddYears(-16),
+                EducationForm.Fulltime,
+                50,
+                60,
+                70);
+
+            storageMock.Setup(x => x.Get(id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync((Entrant?)null);
+
+            // Act
+            Func<Task> act = () => entrantManager.Edit(id, request, CancellationToken.None);
+
+            // Assert
+            await act.Should().ThrowAsync<InvalidOperationException>()
+                .WithMessage($"*{id}*");
+        }
+
+        /// <summary>
+        /// Обрабатывает ошибку
+        /// </summary>
+        [Fact]
+        public async Task DeleteShouldThrow()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            Func<Task> act = () => entrantManager.Delete(id, CancellationToken.None);
+
+            // Assert
+            await act.Should().ThrowAsync<InvalidOperationException>().WithMessage($"*{id}*");
+        }
+
+        /// <summary>
+        /// Удаление абитуриента
+        /// </summary>
+        [Fact]
+        public async Task DeleteShouldWork()
+        {
+            // Arrange
+            var target1 = CreateEntrant();
+            storageMock.Setup(x => x.Get(It.Is<Guid>(y => y == target1.Id), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(target1);
+
+            // Act
+            await entrantManager.Delete(target1.Id, CancellationToken.None);
+
+            // Assert
+            storageMock.Verify(x => x.Get(It.Is<Guid>(y => y == target1.Id), It.IsAny<CancellationToken>()), Times.Once);
+            storageMock.Verify(x => x.Delete(It.Is<Guid>(y => y == target1.Id), It.IsAny<CancellationToken>()), Times.Once);
+            storageMock.VerifyNoOtherCalls();
+        }
+
+        /// <summary>
+        /// Возвращает правильную статистику
+        /// </summary>
+        [Fact]
+        public async Task GetEntrantStatisticsShouldWork()
+        {
+            // Arrange
+            var target1 = CreateEntrant();
+            var target2 = CreateEntrant();
+            var entrants = new List<Entrant>
+            {
+                target1, target2
+            };
+
+            storageMock.Setup(x => x.GetAll(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(entrants);
+
+            // Act
+            var result = await entrantManager.GetEntrantStatistics(CancellationToken.None);
+
+            // Assert
+            result.Should().BeEquivalentTo(new EntrantStatistics(2, 0));
+        }
+
+        private static Entrant CreateEntrant()
+            => new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Иванов И.И.",
+                Gender = Gender.Male,
+                Birthday = DateTime.Now.AddYears(-18),
+                EducationForm = EducationForm.Fulltime,
+                MathExamScore = 14,
+                RusExamScore = 22,
+                ITExamScore = 11,
+            };
+    }
+}
--- AdmissionCommittee.Storage.InMemory.Tests/EntrantInMemoryStorage.Tests.cs
+++ AdmissionCommittee.Storage.InMemory.Tests/EntrantInMemoryStorage.Tests.cs
@@ -52,9 +52,29 @@ namespace AdmissionCommittee.Storage.InMemory.Tests
                 .And
                 .BeEquivalentTo(new
                 {
-                    Id = target.Id,
-                    Age = 14,
+                    target.Id,
                 });
+        }
+
+        /// <summary>
+        /// Проверяет правильность редактирования абитуриента
+        /// </summary>
+        [Fact]
+        public async Task EditShouldWork()
+        {
+            // Arrange
+            var target1 = CreateEntrant();
+            var target2 = CreateEntrant();
+            target2.Name = "Петров П.П.";
+            await storage.Add(target1, CancellationToken.None);
+
+            // Act
+            await storage.Edit(target1.Id, target2, CancellationToken.None);
+            var result = await storage.Get(target1.Id, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(target2, options => options.Excluding(x => x.Id));
         }
 
         /// <summary>
@@ -81,7 +101,7 @@ namespace AdmissionCommittee.Storage.InMemory.Tests
             await storage.Add(target, CancellationToken.None);
 
             // Act
-            Func<Task> act = () => storage.Delete(Guid.NewGuid(), CancellationToken.None);
+            Func<Task> act = () => storage.Delete(target.Id, CancellationToken.None);
 
             // Assert
             await act.Should().NotThrowAsync();
@@ -90,19 +110,27 @@ namespace AdmissionCommittee.Storage.InMemory.Tests
         }
 
         /// <summary>
-        /// 
+        /// Проверяет получение всех абитуриентов
         /// </summary>
         [Fact]
         public async Task GetAllShouldReturnValues()
         {
+            // Arrange
             var target = CreateEntrant();
             await storage.Add(target, CancellationToken.None);
 
+            // Act
+            var result = await storage.GetAll(CancellationToken.None);
+
+            // Assert
+            result.Should().HaveCount(2)
+                .And.ContainSingle(x => x.Id == target.Id);
         }
 
         private static Entrant CreateEntrant()
             => new()
             {
+                Id = Guid.NewGuid(),
                 Name = "Иванов И.И.",
                 Gender = Gender.Male,
                 Birthday = DateTime.Now.AddYears(-18),
